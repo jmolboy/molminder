3c2caff7151b7890b6aa45cc6544456a827db234
jmolboy <johnnymol@163.com>
2016-05-23 18:25:37 +0800

    添加文件操作功能
    添加文件的操作，打开、保存

---
+++ app.js
@@ -0,0 +1,64 @@
+function initEditor(){
+	angular.module('kityminderDemo', ['kityminderEditor'])
+			.controller('MainController', function($scope) {
+				$scope.initEditor = function(editor, minder) {
+					window.editor = editor;
+					window.minder = minder;
+				};
+	});	
+}
+
+initEditor();//init editor
+
+let filepath='';
+
+const ipc =  Mrequire('electron').ipcRenderer;
+
+//新建文件
+ipc.on('new-file', function (event, path) {
+	//原文件保存
+	console.log('create new file');
+})
+
+
+//保存文件
+ipc.on('save-file', function (event, path) {
+	if (!path) {
+		//提醒
+		// ipc.send('save-file-nopath');
+		return;
+	}
+	
+	var source=editor.minder.exportJson();
+	var source_str=JSON.stringify(source);
+
+	filepath=path;
+	var fs = Mrequire('fs');
+	fs.writeFileSync(path,source_str,'utf8');
+})
+
+
+//打开文件
+ipc.on('open-file', function (event, path) {
+	if (!path) {
+		//提醒
+		ipc.send('open-file-nopath');
+		return;
+	}
+	
+	var fs = Mrequire('fs');
+	fs.readFile(path, 'utf8', function (err, data) {
+	    if (err) {
+			// ipc.send('open-file-nopath');
+	    	return;
+	    }
+		filepath=path;
+		try{
+		    var obj = JSON.parse(data);
+			editor.minder.importJson(obj);
+		}
+		catch(e){
+			ipc.send('file-content-error');
+		}
+	});
+})
--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const {BrowserWindow} = electron;
 const Menu = electron.Menu;
 const Tray = electron.Tray;
 
+const ipc = require('electron').ipcMain;
+const dialog = require('electron').dialog;
+
 var appIcon = null;
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -14,44 +17,321 @@ var appIcon = null;
 let win;
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+    // Create the browser window.
+    win = new BrowserWindow({width: 1238, height: 770, frame: true});
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`);
+    // and load the index.html of the app.
+    win.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+    // Open the DevTools.
+    win.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+    // Emitted when the window is closed.
+    win.on('closed', function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null;
+    });
 }
 
+let filepath = '';//文件路径
+
+/*create menu*/
+let template = [{
+    label: '文件',
+    submenu: [{
+        label: '新建',
+        accelerator: 'CmdOrCtrl+N',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                win.webContents.send('new-file', '');
+            }
+        }
+    }, {
+        label: '打开',
+        accelerator: 'CmdOrCtrl+O',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                const options = {
+                    title: '打开',
+                    filters: [
+                        {name: 'Json', extensions: ['json']}
+                    ],
+                    properties: ['openFile', 'openDirectory']
+                }
+
+                dialog.showOpenDialog(options, function (files) {
+                    if (!files && files.length < 1) {
+                        dialog.showErrorBox('错误', '请选择要打开的文件')
+                        return;
+                    }
+
+                    filepath = files[0];
+                    win.webContents.send('open-file', filepath);
+                })
+            }
+        }
+    }, {
+        label: '保存',
+        accelerator: 'CmdOrCtrl+S',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                const options = {
+                    title: '保存脑图',
+                    filters: [
+                        {name: 'Json', extensions: ['json']}
+                    ]
+                }
+
+                if (filepath != '') {
+                    win.webContents.send('save-file', filepath);
+                }
+                else {
+                    dialog.showSaveDialog(options, function (filename) {
+                        filepath = filename;
+                        win.webContents.send('save-file', filepath);
+                    })
+                }
+            }
+        }
+    }]
+}, {
+    label: '修改',
+    submenu: [{
+		label: '撤销',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+    }, {
+        label: '重做',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+    }, {
+        type: 'separator'
+    }, {
+        label: '剪贴',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+    }, {
+        label: '拷贝',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+    }, {
+        label: '粘贴',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+    }, {
+        label: '全选',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+    }]
+}, {
+    label: '视图',
+    submenu: [{
+        label: '重新加载',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                // on reload, start fresh and close any old
+                // open secondary windows
+                if (focusedWindow.id === 1) {
+                    BrowserWindow.getAllWindows().forEach(function (win) {
+                        if (win.id > 1) {
+                            win.close()
+                        }
+                    })
+                }
+                focusedWindow.reload()
+            }
+        }
+    }, {
+        label: '全屏幕',
+        accelerator: (function () {
+            if (process.platform === 'darwin') {
+                return 'Ctrl+Command+F'
+            } else {
+                return 'F11'
+            }
+        })(),
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+            }
+        }
+    }, {
+        label: '开发者工具',
+        accelerator: (function () {
+            if (process.platform === 'darwin') {
+                return 'Alt+Command+I'
+            } else {
+                return 'Ctrl+Shift+I'
+            }
+        })(),
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                focusedWindow.toggleDevTools()
+            }
+        }
+    }, {
+        type: 'separator'
+    }, {
+        label: 'App Menu Demo',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                const options = {
+                    type: 'info',
+                    title: 'Application Menu Demo',
+                    buttons: ['Ok'],
+                    message: 'This demo is for the Menu section, showing how to create a clickable menu item in the application menu.'
+                }
+                electron.dialog.showMessageBox(focusedWindow, options, function () {
+                })
+            }
+        }
+    }]
+}, {
+    label: '窗口',
+    role: 'window',
+    submenu: [{
+        label: '最小化',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+    }, {
+        label: '关闭',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+    }]
+}, {
+    label: '帮助',
+    role: 'help',
+    submenu: [{
+        label: '更多',
+        click: function () {
+            electron.shell.openExternal('http://electron.atom.io')
+        }
+    }]
+}]
+
+function addUpdateMenuItems(items, position) {
+    const version = electron.app.getVersion()
+    let updateItems = [{
+        label: 'Version '+version,
+        enabled: false
+    }, {
+        label: '检查更新',
+        enabled: false,
+        key: 'checkingForUpdate'
+    }, {
+        label: '检查更新',
+        visible: false,
+        key: 'checkForUpdate',
+        click: function () {
+            require('electron').autoUpdater.checkForUpdates()
+        }
+    }, {
+        label: '重启以安装更新',
+        enabled: true,
+        visible: false,
+        key: 'restartToUpdate',
+        click: function () {
+            require('electron').autoUpdater.quitAndInstall()
+        }
+    }]
+
+    items.splice.apply(items, [position, 0].concat(updateItems))
+}
+
+if (process.platform === 'darwin') {
+    const name = electron.app.getName()
+    template.unshift({
+        label: name,
+        submenu: [{
+            label: `关于 ${name}`,
+            role: 'about'
+        }, {
+            type: 'separator'
+        }, {
+            label: '偏好设置',
+            role: 'services',
+            submenu: []
+        }, {
+            type: 'separator'
+        }, {
+            label: `隐藏 ${name}`,
+            accelerator: 'Command+H',
+            role: 'hide'
+        }, {
+            label: '隐藏其它',
+            accelerator: 'Command+Alt+H',
+            role: 'hideothers'
+        }, {
+            label: '显示全部',
+            role: 'unhide'
+        }, {
+            type: 'separator'
+        }, {
+            label: '退出',
+            accelerator: 'Command+Q',
+            click: function () {
+                app.quit()
+            }
+        }]
+    })
+    // Window menu.
+    template[3].submenu.push({
+        type: 'separator'
+    }, {
+        label: 'Bring All to Front',
+        role: 'front'
+    })
+
+    addUpdateMenuItems(template[0].submenu, 1)
+}
+
+if (process.platform === 'win32') {
+    const helpMenu = template[template.length - 1].submenu
+    addUpdateMenuItems(helpMenu, 0)
+}
 
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', function () {
+    createWindow();
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin')
+    {
+        app.quit();
+    }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null)
+    {
+        createWindow();
+    }
 });
+
+
+ipc.on('save-file-nopath', function (event) {
+    dialog.showErrorBox('错误', '请选择要保存的路径')
+})
+
+ipc.on('open-file-nopath', function (event) {
+    dialog.showErrorBox('错误', '请选择要打开的文件')
+})
+
+ipc.on('file-content-error', function (event) {
+    dialog.showErrorBox('错误', '文件内容无法识别')
+})
+
